23ffea329f145d310673684ec212c1108fb25993
20230910086 <20230910086>
2024-12-26 21:20:09 +0700

    Initial commit: menambahkan file Latihan_08_a.php

---
+++ Latihan_08_menu.php
@@ -0,0 +1,8 @@
+<aside class="col-md-2 p-0">
+    <nav class="nav flex-column bg-light p-3 m-0">
+        <a class="nav-link active" herf="?menu=a">Latihan_08_a.php</a>
+        <a class="nav-link active" herf="?menu=a">Latihan_08_b.php</a>
+        <a class="nav-link active" herf="?menu=a">Latihan_08_c.php</a>
+        <a class="nav-link active" herf="?menu=a">Latihan_08_d.php</a>
+</nav>
+</aside>
